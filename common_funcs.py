87b50f50dbfc8155e61e004fee49a83305ca7434
SushmitaDas09 <sushmitadas2021.5@gmail.com>
2021-10-21 18:32:58 -0400

    added commonfuncs.py

---
+++ common_funcs.py
@@ -0,0 +1,27 @@
+import torch, numpy as np
+
+def diceLoss(result, target):
+	# batchSize = result.shape[2]
+	# inter = torch.sum(torch.sum(result*target, dim = 2), dim = 1)
+	# union = torch.sum(torch.sum(result+target, dim = 2), dim = 1)
+	# dice = torch.sum((2*inter)/union)/batchSize
+	inter = torch.sum(result*target)
+	union = torch.sum(result+target)
+	dice = (2*inter)/union
+	return(1-dice)
+
+def dice(result, target):
+	inter = np.sum(result*target)
+	union = np.sum(result+target)
+	dice = (2*inter)/union
+	return(dice)
+
+def performance(result, target):
+	TP = np.sum(result*target)
+	TN = np.sum((1-result)*(1-target))
+	FP = np.sum(result*(1-target))
+	FN = np.sum((1-result)*target)
+	e = TP/(TP+FP)
+	d = TP/(TP+FN)
+	F = (2*e*d)/(e+d)
+	return([e, d, F])
