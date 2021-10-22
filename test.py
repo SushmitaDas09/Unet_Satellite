2c7d00cfadbbbb5adcb6e0a727f93ef8054198de
SushmitaDas09 <63927475+SushmitaDas09@users.noreply.github.com>
2021-10-21 23:00:03 -0400

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ for file in files:
         img_path = os.path.join(path, file)
         im = cv2.imread(img_path)
         file1 = ('new_'+ (file.split(".png"))[0] + '_marker_mask.png')
-        img_path1 = os.path.join(path1, file)
+        img_path1 = os.path.join(path1, file1)
         gt = cv2.imread(img_path1)
         [h,w,c] = im.shape
         print('Input image size:', [w,h])
